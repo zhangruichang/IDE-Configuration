9d48ae3baa2bad8eff1f9c9105401525ec496ec6
root <root@zhangruichang-test-dev001-shgq.qiyi.virtual>
2015-09-08 12:44:00 +0800

    remove pycharm system

--- Pycharm/system/python_stubs/-793569567/numpy/core/_dummy.py
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-# module numpy.core._dummy
-# from D:\Program files\Python27\lib\site-packages\numpy\core\_dummy.pyd
-# by generator 1.136
-# no doc
-# no imports
-
-# no functions
-# no classes
--- Pycharm/system/python_stubs/-793569567/tri.py
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-# module tri
-# from D:\Program files\Python27\lib\site-packages\matplotlib\_tri.pyd
-# by generator 1.136
-""" Module for unstructured triangular grids """
-# no imports
-
-# no functions
-# no classes
